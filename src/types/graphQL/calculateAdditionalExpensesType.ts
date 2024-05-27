acab4d68df0c71371ee7b7cc93290ffb0f9a0ecf
oykos-development-hub <hub@oykos.me>
2024-05-27 08:06:27 +0000

    Update code from GitLab

--- src/types/graphQL/calculateAdditionalExpensesType.ts
+++ src/types/graphQL/calculateAdditionalExpensesType.ts
@@ -17,10 +17,10 @@ export interface CalculatedAdditionalExpensesItem {
 export interface CalculateAdditionalExpensesParams {
   tax_authority_codebook_id: number | null;
   municipality_id: number | null;
-  net_price?: string | null;
-  gross_price?: string | null;
-  previous_income_net?: string | null;
-  previous_income_gross?: string | null;
+  net_price?: string | null | number;
+  gross_price?: string | null | number;
+  previous_income_net?: string | null | number;
+  previous_income_gross?: string | null | number;
 }
 
 export type CalculateAdditionalExpensesResponse = {
--- src/types/graphQL/receivablesTypes.ts
+++ src/types/graphQL/receivablesTypes.ts
@@ -6,7 +6,7 @@ export interface PaymentOrderParams {
   page: number;
   size: number;
   year: string | null;
-  supplier_id: number | null;
+  supplier_id?: number | null;
 }
 
 export interface Items {
